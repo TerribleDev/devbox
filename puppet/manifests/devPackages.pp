53a3f372d3cc86fd67819e42a1d66c254d2f1d15
Tommy Parnell <parnell.tommy@hotmail.com>
2014-06-24 23:46:29 -0400

    adding more packages

--- puppet/manifests/devPackages.pp
+++ puppet/manifests/devPackages.pp
@@ -1,5 +1,5 @@
 class devPackages {
-	$Packages = ['cowsay', 'ruby', 'rubygems', 'nodejs', 'npm', 'nginx', 'irssi', 'git', 'golang']
+	$Packages = ['cowsay', 'ruby', 'rubygems', 'nodejs', 'npm', 'nginx', 'irssi', 'git', 'golang', 'tmux', 'geany' ]
   
 
   package { $Packages: 
