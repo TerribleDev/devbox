9bd8a236960129f601f38d1cf302da422bd89e5d
Tommy Parnell <parnell.tommy@hotmail.com>
2014-08-08 17:30:41 -0400

    remove ruby

--- puppet/manifests/devPackages.pp
+++ puppet/manifests/devPackages.pp
@@ -1,5 +1,5 @@
 class devPackages {
-	$Packages = ['cowsay', 'ruby', 'nodejs', 'npm', 'nginx', 'irssi', 'git', 'golang', 'tmux', 'geany' ]
+	$Packages = ['cowsay', 'nodejs', 'npm', 'nginx', 'irssi', 'git', 'golang', 'tmux', 'geany' ]
   
 
   package { $Packages: 
